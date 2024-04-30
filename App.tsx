23be6836be35aae13b1f21869019ab1d19a4c478
singarajusaiteja <134045265+singarajusaiteja@users.noreply.github.com>
2024-04-30 21:16:48 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import { SafeAreaView, ScrollView } from 'react-native'
+import React from 'react'
+import FlatCards from './components/FlatCards'
+import ElevatedCards from './components/ElevatedCards'
+import FancyCards from './components/FancyCards'
+import ActionCard from './components/ActionCard'
+import ContactList from './components/ContactList'
+
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+      <FlatCards />
+      <ElevatedCards />
+      <FancyCards />
+      <ContactList/>
+      <ActionCard />
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+export default App
